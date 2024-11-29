7af9d6fc58edce42ab13aa933193e5bd65c51118
Benjamin Doll <benjamin@benji.to>
2024-11-29 03:15:24 -0500

    Move CSS and JS to components

---
+++ js/index.js
@@ -0,0 +1,75 @@
+const generateBtn = document.getElementById('generateBtn');
+const downloadBtn = document.getElementById('downloadBtn');
+const qrCanvas = document.getElementById('qrCanvas');
+const inputText = document.getElementById('inputText');
+const imageSizeSelect = document.getElementById('imageSizeSelect');
+const fileFormatSelect = document.getElementById('fileFormatSelect');
+
+generateBtn.addEventListener('click', () => {
+    const text = inputText.value;
+
+    // Clear previous QR code
+    qrCanvas.style.display = 'none';
+
+    if (text.trim()) {
+        // Generate QR code on canvas
+        QRCode.toCanvas(qrCanvas, text, { width: 512 }, (error) => {
+            if (error) {
+                console.error(error);
+                alert('Error generating QR Code. Please refresh the page and try again.');
+            } else {
+                qrCanvas.style.display = 'block';
+                fileFormatSelect.style.display = 'inline-block';
+                downloadBtn.style.display = 'inline-block';
+            }
+        });
+    } else {
+        alert('Please enter some text or a URL.');
+    }
+});
+
+fileFormatSelect.addEventListener('change', () => {
+    fileFormatSelect.value === 'png' ? imageSizeSelect.style.display = 'inline-block' : imageSizeSelect.style.display = 'none';
+})
+
+downloadBtn.addEventListener('click', () => {
+    const selectedSize = parseInt(imageSizeSelect.value, 10); // Get the selected size
+    const selectedFormat = fileFormatSelect.value; // Get the selected file format
+
+    if (selectedFormat === 'png') {
+        // Create a temporary canvas for the download
+        const tempCanvas = document.createElement('canvas');
+        tempCanvas.width = selectedSize;
+        tempCanvas.height = selectedSize;
+
+        // Generate the QR code on the temporary canvas
+        QRCode.toCanvas(tempCanvas, inputText.value, { width: selectedSize }, (error) => {
+            if (error) {
+                console.error(error);
+                alert('Error generating the QR Code for download.');
+                return;
+            }
+            // Convert the temporary canvas to a PNG image and trigger download
+            const link = document.createElement('a');
+            link.href = tempCanvas.toDataURL('image/png');
+            link.download = 'openQR_' + getCurrentTimestamp() + '_' + selectedSize + 'px' + '.png'; // Filename to download
+            link.click();
+        });
+    } else if (selectedFormat === 'svg') {
+        // Generate the QR code as an SVG
+        QRCode.toString(inputText.value, { type: 'svg', width: selectedSize }, (error, svgString) => {
+            if (error) {
+                console.error(error);
+                alert('Error generating the QR Code for download.');
+                return;
+            }
+
+            // Create a Blob for the SVG and trigger download
+            const blob = new Blob([svgString], { type: 'image/svg+xml' });
+            const link = document.createElement('a');
+            link.href = URL.createObjectURL(blob);
+            link.download = 'openQR_' + getCurrentTimestamp() + '.svg'; // Filename to download
+            link.click();
+        });
+    }
+});
